dda577ed415f36a68c9461eae716644ab6315b18
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-27 15:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let div=document.getElementById("output");
 let prom=new Promise((resolve)=>{
 	setTimeout(() => {
-		resolve("Hello, world");
+		resolve("Hello, world!");
 	},1000)
 })
 prom.then((data) => {
