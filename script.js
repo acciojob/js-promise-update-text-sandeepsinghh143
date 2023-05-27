2a741813321de78b2f0f55fa5135332d6114fe1c
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-27 15:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let div=document.getElementById("output");
+let prom=new Promise((resolve)=>{
+	setTimeout(() => {
+		resolve("Hello, world");
+	},1000)
+})
+prom.then((data) => {
+	div.innerText=data;
+})
